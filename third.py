ceff1c561f4559db349bbe89794f83a00382ec7d
Lyroox <101668067+Lyroox@users.noreply.github.com>
2024-10-17 14:53:02 +0000

    prvočíslo

--- third.py
+++ third.py
@@ -1,31 +1,30 @@
+import math
+
 def je_prvocislo(cislo):
-    """
-    Funkce overi, zda zadane cislo je nebo neni prvocislo a vrati True nebo False
-
-    Prvocislo je takove cislo vetsi nez 1, ktere neni delitelne zadnym jinym cislem jenom 1 a samo sebou.
-
-    Napoveda jak otestova prvocislo:
-    Cislo 36 vznikne nasobenim:
-    1 * 36
-    2 * 18
-    3 * 12
-    4 * 9
-    6 * 6
-    9 * 4
-    12 * 3
-    18 * 2
-    36 * 1
-    Jak vidite v druhe polovine se dvojice opakuji, tzn. v tomto pripade staci overit delitelnost pouze do 6 (vcetne)
-    """
-    return False
+    #funkce zkontroluje, jestli je číslo prvočíslo, a podle toho vrací True nebo False
+    
+    if cislo <= 1:
+        return False  #číslo 1 a menší nejsou prvočísla
+    
+    delitel_limit = int(math.sqrt(cislo))
+    for delitel in range(2, delitel_limit + 1):
+        if cislo % delitel == 0:
+            return False  #nalezen dělitel čísla, tedy číslo není prvočíslo
+    
+    return True  #číslo je prvočíslo
 
 def vrat_prvocisla(maximum):
-    """
-    Funkce spocita vsechna prvocisla v rozsahu 1 az maximum a vrati je jako seznam.
-    """
-    return [2,3,5]
+    #funkce spočítá všechna prvočísla v rozsahu 1 až maximum a vrátí je jako seznam
+    
+    prvocisla = []
+    for cislo in range(1, maximum + 1):
+        if je_prvocislo(cislo):
+            prvocisla.append(cislo)
+    return prvocisla
+
 
 if __name__ == "__main__":
-    cislo = input("Zadej maximum: ")
-    prvocisla = vrat_prvocisla(cislo)
-    print(prvocisla)
+    uzivatel_cislo = int(input("Zadej maximální číslo pole: "))
+    prvocisla = vrat_prvocisla(uzivatel_cislo)
+
+    print(f"Prvočísla od 1 do {uzivatel_cislo}: {prvocisla}")
