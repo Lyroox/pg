143d58c67423e121c4c6bfe95c29a69e2a825d37
Lyroox <101668067+Lyroox@users.noreply.github.com>
2025-01-16 09:52:20 +0000

    kód3

--- zkouska3.py
+++ zkouska3.py
@@ -19,6 +19,28 @@ class Shape():
 
 
 # ZDE NAPIŠTE VÁŠ KÓD
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        super().__init__("Rectangle")
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return round(self.width * self.height, 1)
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        super().__init__("Circle")
+        self.radius = radius
+
+    def area(self):
+        import math
+        return round(math.pi * self.radius ** 2, 1)
+
+    def __str__(self):
+        return f"{self.shape_name} with a radius of {self.radius} has an area of {self.area()}"
+
 
 
 from unittest.mock import patch, MagicMock, mock_open
